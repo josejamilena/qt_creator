81a9909da47f8f462c8ae1d6dbaa7f5540c53a27
Jose Antonio <jose@kabru>
2014-01-17 10:01:25 +0100

    Inicio

---
+++ untitled/main.cpp
@@ -0,0 +1,15 @@
+#include <QApplication>
+#include "html5applicationviewer.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    Html5ApplicationViewer viewer;
+    viewer.setOrientation(Html5ApplicationViewer::ScreenOrientationAuto);
+    viewer.showExpanded();
+    viewer.showFullScreen();
+    viewer.loadFile(QLatin1String("html/index.html"));
+
+    return app.exec();
+}
